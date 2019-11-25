ddba2a16bcfc0902cec524497d6ae9cb26284b26
Adam_Bates <Creizai@EMAIL.COM>
2019-11-24 20:40:50 -0500

    Added shuffle Function

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,66 @@
+class Card {
+  constructor (rank, suit, value) {
+    this.rank = rank
+    this.suit = suit
+    this.value = value
+  }
+}
+
+class Deck {
+  constructor() {
+    this.cards = []
+    
+    let ranks = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "Jack",
+      "Queen",
+      "King",
+      "Ace"
+    ]
+    
+    let suits = [
+      "Heart",
+      "Club",
+      "Diamond",
+      "Spade"
+    ]
+  
+    for (let i = 0; i < ranks.length; i++) {
+      for (let j = 0; j < suits.length; j++) {
+        let rank = ranks[i]
+        let suit = suits[j]
+        let value = i
+        
+        this.cards.push(new Card(rank, suit, value))
+      }   
+    }
+  }
+}
+
+//  Fisher-Yates shuffle
+// Walks array in reverse order & swap each element with a random one before it. 
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+
+    // swap elements array[i] and array[j]
+    // we use "destructuring assignment" syntax to achieve that
+    // you'll find more details about that syntax in later chapters
+    // same can be written as:
+    // let t = array[i]; array[i] = array[j]; array[j] = t
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+const deck = new Deck()
+
+console.log(deck)
 console.log("hello!");
